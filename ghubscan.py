ef9314943d68fdcf38c437cc50436e63e4d41be4
ovax <181485458+banaxou@users.noreply.github.com>
2024-11-11 17:20:38 +0100

    Update ghubscan.py

--- ghubscan.py
+++ ghubscan.py
@@ -1,11 +1,13 @@
 import os,requests,fade
-os.system('cls' if os.name == 'nt' else 'clear') 
+
+os.system('cls' if os.name == 'nt' else 'clear')
 R = "\033[0m"
 red = "\033[31m"
 green = "\033[32m"
 
-v = ['code by ovax','https://github.com/banaxou']
-ve = "v1.0"
+v = ['code by ovax', 'https://github.com/banaxou',"v1.1"]
+
+
 ba = f"""
  ▄▄ •  ▄ .▄▄• ▄▌▄▄▄▄· .▄▄ ·  ▄▄·  ▄▄▄·  ▐ ▄ 
 ▐█ ▀ ▪██▪▐██▪██▌▐█ ▀█▪▐█ ▀. ▐█ ▌▪▐█ ▀█ •█▌▐█
@@ -14,68 +16,87 @@ ba = f"""
 ·▀▀▀▀ ▀▀▀ · ▀▀▀ ·▀▀▀▀  ▀▀▀▀ ·▀▀▀  ▀  ▀ ▀▀ █▪
 
 
-       {green}{ve}{R}                 {red}{v[0]}{R} | {green}{v[1]}{R} 
-                        
+       {green}{v[2]}{R}                 {red}{v[0]}{R} | {green}{v[1]}{R}
 """
 print(fade.purplepink(ba))
 user = input(f"[{red}+{R}] {green}Enter GitHub username{R}: ")
 
 try:
-
     apix = requests.get(f"https://api.github.com/users/{user}")
-    re = requests.get(f"https://api.github.com/users/{user}/events/public")
-
-    if apix.status_code == 200:
-        if re.status_code == 200:
-            print(f"{green}request 200{R}")
-            data = re.json()
-            email_found = False
-            w = 50
-            print("Scanning...")
-
-            for event in data:
-                if 'payload' in event and 'commits' in event['payload']:
-                    for commit in event['payload']['commits']:
-                        if 'author' in commit and 'email' in commit['author']:
-                            email = commit['author']['email']
-                            print(f"╔{'═' * w}╗")
-                            print(f"{red}email:{R} {email}")
-                            email_found = True
-                            break
-                if email_found:
-                    break
-            
-
-            data = apix.json()
-            user_info = {
-                'Pseudo': data.get('login', 'Not found'),
-                'Icon': data.get('avatar_url', 'Not found'),
-                'URL': data.get('html_url', 'Not found'),
-                'Name': data.get('name', 'Not found'),
-                'Type': data.get('type', 'Not found'),
-                'ID': data.get('id', 'Not found'),
-                'Location': data.get('location', 'Not found'),
-                'Following': data.get('following', 'Not found'),
-                'Followers': data.get('followers', 'Not found'),
-                'Created at': data.get('created_at', 'Not found'),
-                'Updated at': data.get('updated_at', 'Not found'),
-                'Repositories': data.get('public_repos', 'Not found'),
-                'Public Gists': data.get('public_gists', 'Not found'),
-                'Twitter': data.get('twitter_username', 'Not found'),
-            }
-
-
-            for key, value in user_info.items():
-                print(f"{red}{key}:{R} {value}")
-            print(f"╚{'═' * w}╝")
-
-        if not email_found:
-            print(f"{red}Email not found.{R}")
     
-    elif apix.status_code == 404:
-        print(f"{red}User not found.{R}")
+    if apix.status_code == 200:
+        print(f"{green}Request 200 - {R}")
+        data = apix.json()
+        w = 50
+        print(f"╔{'═' * w}╗")
+        user_info = {
+            'Pseudo': data.get('login', 'Not found'),
+            'Icon': data.get('avatar_url', 'Not found'),
+            'URL': data.get('html_url', 'Not found'),
+            'Name': data.get('name', 'Not found'),
+            'Type': data.get('type', 'Not found'),
+            'ID': data.get('id', 'Not found'),
+            'Location': data.get('location', 'Not found'),
+            'Following': data.get('following', 'Not found'),
+            'Followers': data.get('followers', 'Not found'),
+            'Created at': data.get('created_at', 'Not found'),
+            'Updated at': data.get('updated_at', 'Not found'),
+            'Repositories': data.get('public_repos', 'Not found'),
+            'Public Gists': data.get('public_gists', 'Not found'),
+            'Twitter': data.get('twitter_username', 'Not found'),
+        }
+
+        for key, value in user_info.items():
+            print(f"{red}{key}:{R} {value}")
+        
+        print(f"╚{'═' * 50}╝")
+        
+        repos_url = f"https://api.github.com/users/{user}/repos?per_page=5"
+        repos_data = requests.get(repos_url).json()
+
+        print(f"\n{green}Public Repositories{R}:")
+        for repo in repos_data:
+            print(f"- {repo['name']} - {repo['html_url']}")
+
+        gists_url = f"https://api.github.com/users/{user}/gists?per_page=5"
+        gists_data = requests.get(gists_url).json()
+
+        print(f"\n{green}Public Gists{R}:")
+        for gist in gists_data:
+            print(f"- {gist['description']} - {gist['html_url']}")
+
+        follo_url = f"https://api.github.com/users/{user}/followers?per_page=5"
+        follo_data = requests.get(follo_url).json()
+
+        print(f"\n{green}Followers{R}:")
+        for follower in follo_data:
+            print(f"- {follower['login']} - {follower['html_url']}")  
+
+        follo_url = f"https://api.github.com/users/{user}/following?per_page=5"
+        follo_data = requests.get(follo_url).json()
+
+        print(f"\n{green}Following{R}:")
+        for following in follo_data:
+            print(f"- {following['login']} - {following['html_url']}")  
+
+
+        or_url = f"https://api.github.com/users/{user}/orgs"
+        or_data = requests.get(or_url).json()
+
+        print(f"\n{green}Organizations{R}:")
+        if not or_data:
+            print(f"{red}No organizations found{R}")
+        for org in or_data:
+
+            org_url = org.get('html_url', 'Not found')
+            print(f"- {org['login']} - {org_url}")
+
+        twitter_user = data.get('twitter_username', None)
+        if twitter_user:
+            print(f"\n{green}Twitter Profile: {R}https://x.com/{twitter_user}")
+
     else:
-        print(f"Response: {apix.status_code} ;(")
+        print(f"{red}API error: {apix.status_code}{R}")
 
 except requests.RequestException as e:
     print(f"{red}Network error: {e}{R}")
