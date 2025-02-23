74d51f57d42476912354af03d9a356d64c7f8cc5
ovax <181485458+banaxou@users.noreply.github.com>
2025-02-23 20:11:38 +0100

    Update ghubscan.py

--- ghubscan.py
+++ ghubscan.py
@@ -10,7 +10,6 @@ w = 50
 agent = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
-# im tired
 def idéfix(agent):
 
     you = input(f"[{red}+{R}] {green}Enter GitHub user ID{R}: ") or "181485458"
